36c00e07aec56e1805876d81b55520911bac1bde
Santu De <Santu.De@inindcrws.com>
2023-03-29 18:04:13 +0530

    Router Define

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import '../styles/App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path :'/',
+    element : <div>Root Element</div>
+  },
+  {
+    path:'/quiz',
+    element:<div>Quiz Component</div>
+  },
+  {
+    path:'/result',
+    element:<div>Result components</div>
+  },
+
+])
 
 function App() {
   return (
-    <div className="App">
-      <h2>React app</h2>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
