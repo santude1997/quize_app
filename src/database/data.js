0c27144a0eaa41d76516c2d6e9b37b5681cf7a70
Santu De <Santu.De@inindcrws.com>
2023-03-31 20:40:15 +0530

    Database created and acess through useeffect

---
+++ src/database/data.js
@@ -0,0 +1,32 @@
+export default[
+    {
+        id: 1,
+        question: "JavaScript is an _________________ language",
+        option: [
+            'object-oriented1',
+            'object-oriented2',
+            'object-oriented3',
+            'object-oriented4'
+        ] 
+    },
+    {
+        id: 2,
+        question: "JavaScript is an _____ language",
+        option: [
+            'object-oriented1',
+            'object-oriented2',
+            'object-oriented3',
+            'object-oriented4'
+        ] 
+    },
+    {
+        id: 3,
+        question: "JavaScript is an __________white_ language",
+        option: [
+            'object-oriented1',
+            'object-oriented2',
+            'object-oriented3',
+            'object-oriented4'
+        ] 
+    }
+]
